9b8ab120157935ce5ebae4ff3f1ae38492054d03
matimasman <matimasman@gmail.com>
2022-07-17 00:28:09 -0300

    update

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,6 +1,12 @@
 function index(req, res) {
-
-    res.render('tasks/index');
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM tasks', (err, tasks) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('tasks/index', { tasks });
+        });
+    });
 }
 
 function create(req, res) {
@@ -13,7 +19,41 @@ function store(req, res) {
 
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO tasks SET ?', [data], (err, rows) => {
-            console.log(rows);;
+            res.redirect('/tasks');
+        });
+    });
+}
+
+function destroy(req, res) {
+    const id = req.body.id;
+
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM tasks WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/tasks');
+        });
+    });
+}
+
+function edit(req, res) {
+    const id = req.params.id;
+
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM tasks WHERE id = ?', [id], (err, tasks) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('tasks/edit', { tasks });
+        });
+    });
+}
+
+function update(req, res) {
+    const id = req.params.id;
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE tasks SET ? WHERE id = ?', [data, id], (err, rows) => {
+            res.redirect('/tasks');
         });
     });
 }
@@ -22,4 +62,7 @@ module.exports = {
     index: index,
     create: create,
     store: store,
+    destroy: destroy,
+    edit: edit,
+    update: update,
 }
